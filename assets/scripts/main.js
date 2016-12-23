87e98f13d2b4d2d86602f503c726fb5fa6ccda09
Mykolas Raizys <hello@rmykolas.me>
2016-12-23 13:05:47 -0500

    Tekstas

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,7 +18,8 @@
     // All pages
     'common': {
       init: function() {
-        // JavaScript to be fired on all pages
+        window.sr = ScrollReveal();
+        sr.reveal('.reveal');
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
